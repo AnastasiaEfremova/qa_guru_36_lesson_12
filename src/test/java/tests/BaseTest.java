969737bf6a707dba9c7d4d27c65adcb97e1b0f3d
Anastasia Efremova <anastasia.stoloto@gmail.com>
2025-09-09 21:44:30 +0300

    3 Part Homework 12.

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -12,17 +12,27 @@ public class BaseTest {
 
     @BeforeAll
     static void setupConfig() {
-            Configuration.baseUrl = "https://demoqa.com";
-            Configuration.timeout = 10000;
-            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                    "enableVNC", true,
-                    "enableVideo", true
-            ));
-            Configuration.browserCapabilities = capabilities;
+        String browser = System.getProperty("browser", "chrome");
+        String browserVersion = System.getProperty("browserVersion", "127.0");
+        String browserSize = System.getProperty("browserSize", "1920x1080");
+        String remoteUrl = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
-            SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browser = browser;
+        Configuration.browserVersion = browserVersion;
+        Configuration.browserSize = browserSize;
+        Configuration.remote = remoteUrl;
+        Configuration.timeout = 10000;
+
+        // Настраиваем capabilities для Selenoid
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 }
